0c295d71c1990dd41de1633c5dddf8a7a0f12c4c
Hamburger317 <107779558+Hamburger317@users.noreply.github.com>
2022-07-05 16:27:43 -0700

    Rewrote topmenu.py, condensing it into 1 function with sub functions.

--- src/topmenu.py
+++ src/topmenu.py
@@ -1,18 +1,34 @@
 from tkinter import *
+from menubarFunctions import *
 import windowMain
 
+
+def draw_menubar():
+    menu_bar = Menu(windowMain.window)
+
+    def file_submenu():
+        file_menu = Menu(menu_bar)
+        menu_bar.add_cascade(label = "File")
+        file_menu.add_command(label = "Save", command = save_file)
+        file_menu.add_command(label = "Open", command = open_file)
+
+    file_submenu()
+    windowMain.window.config(menu = menu_bar)
+
+    
+
 #Special thanks to Blue Badger from Grepper :)
-def draw_filemenu():
-    file_menubar = Menu(windowMain.window)
-    filemenu = Menu(file_menubar, tearoff=0)
-    filemenu.add_command(label="New - Ctrl + N")
-    filemenu.add_command(label="Open - Ctrl + O")
-    filemenu.add_command(label="Save - Ctrl + S")
-    filemenu.add_separator()
-    filemenu.add_command(label="Exit", command=windowMain.window.quit)
-    file_menubar.add_cascade(label="File", menu=filemenu)
-    windowMain.window.config(menu=file_menubar)
-    return file_menubar
+# def draw_filemenu():
+#     file_menubar = Menu(windowMain.window)
+#     filemenu = Menu(file_menubar, tearoff=0)
+#     filemenu.add_command(label="New - Ctrl + N")
+#     filemenu.add_command(label="Open - Ctrl + O")
+#     filemenu.add_command(label="Save - Ctrl + S")
+#     filemenu.add_separator()
+#     filemenu.add_command(label="Exit", command=windowMain.window.quit)
+#     file_menubar.add_cascade(label="File", menu=filemenu)
+#     windowMain.window.config(menu=file_menubar)
+#     return file_menubar
 
 # def draw_helpmenu():
 #     menubar = Menu(windowMain.window)
@@ -23,5 +39,4 @@ def draw_filemenu():
 #     menubar.add_cascade(label="Help", menu=helpmenu)
 
 def draw_topbar():
-    draw_filemenu()
-    # draw_helpmenu()
+    draw_menubar()
