01cca3632b9837be2d2c23d31e984f77c03c0913
Hamburger317 <107779558+Hamburger317@users.noreply.github.com>
2022-07-05 16:48:40 -0700

    Removed window.config() as it was redundent.

--- src/windowMain.py
+++ src/windowMain.py
@@ -5,11 +5,10 @@ window = tk.Tk()
 #Constants
 window_title = "Placerholder"
 window_size = "1000x750"
-menubar = topmenu.draw_topbar()
+
 
 def draw_window():
     window.geometry(window_size)
     window.title(window_title)
-    topmenu.draw_topbar()
-    window.config(menu = menubar)
+    topmenu.draw_menubar()
     window.mainloop()
