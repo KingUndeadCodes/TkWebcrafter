7ba8a19a1fc8f2e8ff6f1678bef230574d82f46d
Hamburger317 <107779558+Hamburger317@users.noreply.github.com>
2022-07-05 16:44:26 -0700

    Added "exit" option in File submenu

--- src/topmenu.py
+++ src/topmenu.py
@@ -11,6 +11,7 @@ def draw_menubar():
         menu_bar.add_cascade(label = "File", menu = file_menu)
         file_menu.add_command(label = "Save", command = save_file)
         file_menu.add_command(label = "Open", command = open_file)
+        file_menu.add_command(label = "Exit", command = windowMain.window.quit)
 
     file_submenu()
     windowMain.window.config(menu = menu_bar)
