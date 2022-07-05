9f5b11105c5aba84b696a4b616adf76aae86cfa8
Hamburger317 <107779558+Hamburger317@users.noreply.github.com>
2022-07-04 18:30:51 -0700

    Finally got a way to draw widgets from another file working :DD

---
+++ src/topmenu.py
@@ -0,0 +1,6 @@
+from tkinter import *
+import windowMain
+
+def draw_topbar():
+    menubar = Menu(windowMain.window)
+    return menubar
--- src/windowMain.py
+++ src/windowMain.py
@@ -1,12 +1,15 @@
 import tkinter as tk
-import widgetMain
+import topmenu
 window = tk.Tk()
 
 #Constants
 window_title = "Placerholder"
 window_size = "1000x750"
+menubar = topmenu.draw_topbar()
 
 def draw_window():
     window.geometry(window_size)
     window.title(window_title)
+    topmenu.draw_topbar()
+    window.config(menu = menubar)
     window.mainloop()
