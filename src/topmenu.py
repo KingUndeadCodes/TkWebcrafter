ad01966ba2d65f6e5210e31c2177be4cfecfae0c
Hamburger317 <107779558+Hamburger317@users.noreply.github.com>
2022-07-05 16:31:29 -0700

    Removed older code.

--- src/topmenu.py
+++ src/topmenu.py
@@ -15,28 +15,5 @@ def draw_menubar():
     file_submenu()
     windowMain.window.config(menu = menu_bar)
 
-    
-
-#Special thanks to Blue Badger from Grepper :)
-# def draw_filemenu():
-#     file_menubar = Menu(windowMain.window)
-#     filemenu = Menu(file_menubar, tearoff=0)
-#     filemenu.add_command(label="New - Ctrl + N")
-#     filemenu.add_command(label="Open - Ctrl + O")
-#     filemenu.add_command(label="Save - Ctrl + S")
-#     filemenu.add_separator()
-#     filemenu.add_command(label="Exit", command=windowMain.window.quit)
-#     file_menubar.add_cascade(label="File", menu=filemenu)
-#     windowMain.window.config(menu=file_menubar)
-#     return file_menubar
-
-# def draw_helpmenu():
-#     menubar = Menu(windowMain.window)
-#     helpmenu = Menu(menubar, tearoff=0)
-#     helpmenu.add_command(label="About")
-#     helpmenu.add_command(label="Help - F1")
-#     helpmenu.add_command(label="Keyboard Shortcuts - Ctrl + K")
-#     menubar.add_cascade(label="Help", menu=helpmenu)
-
 def draw_topbar():
     draw_menubar()
