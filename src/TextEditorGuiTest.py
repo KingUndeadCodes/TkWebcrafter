db716c6ffea6a17b1e7916d222b261b2b1704bda
Hamburger317 <107779558+Hamburger317@users.noreply.github.com>
2022-07-06 18:51:52 -0700

    huge changes added to TextEditorGuiTest.py

--- src/TextEditorGuiTest.py
+++ src/TextEditorGuiTest.py
@@ -1,15 +1,30 @@
 from tkinter import *
+from tkinter import filedialog as fd
+from tkinter import messagebox as msgbox
 
 root = Tk()
-root.geometry("100x100")
+root.geometry("200x125")
 root.title("Micro Notepad")
 
-text = Text(root, width = 10)
-button2 = Button(root)
-button2.config(height= 1, width = 2)
-button3 = Button(root)
+def save_file():
+    save_buffer = text.get(1.0, "end")
+    
+
+def open_file():
+    file = fd.askopenfilename()
+    filecontent = open(file, "r")
+    text.insert(1.0, filecontent.read())
+
+def clear():
+
+    text.delete(1.0, "end")
+
+text = Text(root, width = 20, height = 20)
+button2 = Button(root, text = "s", command = save_file)
+button2.config(height= 1, width = 2, )
+button3 = Button(root, text = "o", command = open_file)
 button3.config(height= 1, width = 2)
-button4 = Button(root)
+button4 = Button(root, text = "c", command = clear)
 button4.config(height= 1, width = 2)
 button5 = Button(root)
 button5.config(height= 1, width = 2)
@@ -23,4 +38,5 @@ button2.pack(fill = BOTH, expand = True)
 button3.pack(fill = BOTH, expand = True)
 button4.pack(fill = BOTH, expand = True)
 button5.pack(fill = BOTH, expand = True)
+
 root.mainloop()
