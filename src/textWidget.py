6183aec5f43b9c87e15acae4acc2ca8d172256d1
Hamburger317 <107779558+Hamburger317@users.noreply.github.com>
2022-07-07 20:54:07 -0700

    Added basic code to textWidget.py

--- src/textWidget.py
+++ src/textWidget.py
@@ -1,3 +1,7 @@
 #This file will make the Text widget and the edit buttons.
 from tkinter import Text
+import windowMain
 
+def draw_text():
+    textbox = Text(windowMain.window)
+    textbox.pack(side = "left", padx = 10)
